993fb9dfae28e6fa0720a9ef01909550e0b207bc
Agostino Marzotta <ago1986@gmail.com>
2016-07-22 13:26:22 +0200

    Async call - iOS
    Async call of cordova plugin - no more blocks the main thread

--- src/ios/CDVGeocoder.m
+++ src/ios/CDVGeocoder.m
@@ -16,29 +16,32 @@
     NSString* addressString = [command.arguments objectAtIndex:0];
     
     CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [self.commandDelegate runInBackground:^{ //avoid main thread block  especially if sharing big files from url
+
     
-    [geocoder geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
-        if (error)
-        {
-            NSLog(@"Geocode failed with error: %@", error);
+        [geocoder geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
+            if (error)
+            {
+                NSLog(@"Geocode failed with error: %@", error);
 
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-        } else {
+                CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+                [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+            } else {
 
-            NSLog(@"Received placemarks: %@", placemarks);
+                NSLog(@"Received placemarks: %@", placemarks);
 
-            CLPlacemark *place = [placemarks objectAtIndex:0];
-            NSDictionary *loc = [NSDictionary dictionaryWithObjectsAndKeys:
+                CLPlacemark *place = [placemarks objectAtIndex:0];
+                NSDictionary *loc = [NSDictionary dictionaryWithObjectsAndKeys:
                                     [NSNumber numberWithDouble:place.location.coordinate.latitude], @"latitude",
                                     [NSNumber numberWithDouble:place.location.coordinate.longitude ], @"longitude", nil];
             
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:loc];
-            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-        }
+                CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:loc];
+                [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+            }
         
+        }];
     }];
-         
+     
 }
 
 
